af33eb7473be0c48403315cfc99b9562c5deaa7c
yevhen-shatalov <zhekashatalov1986@gmail.com>
2021-11-20 12:16:35 +0200

    init commit

--- src/com/pb/shatalov/hw8/OnlineShop.java
+++ src/com/pb/shatalov/hw8/OnlineShop.java
@@ -3,7 +3,7 @@ package com.pb.shatalov.hw8;
 public class OnlineShop {
     public static void main(String[] args) throws WrongLoginExeption, WrongPasswordExeption {
         Auth auth = new Auth();
-        System.out.println("Пройдите регистрацию (логин - только латиница и цифры, пароль - только латиница, цифры и занак подчеркивания)");
+        System.out.println("Пройдите регистрацию (логин - только латиница и цифры от 5 до 20 символов, пароль - только латиница, цифры и занак подчеркивания от 5 символов)");
         try {
             auth.signUp();
             System.out.println("Введите ваш логин и пароль");
