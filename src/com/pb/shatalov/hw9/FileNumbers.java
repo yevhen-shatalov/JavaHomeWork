5342dee130e8c829915c8d631f373d4fc9949fee
yevhen-shatalov <zhekashatalov1986@gmail.com>
2021-11-23 12:01:49 +0200

    init commit

---
+++ src/com/pb/shatalov/hw9/FileNumbers.java
@@ -0,0 +1,73 @@
+package com.pb.shatalov.hw9;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FileNumbers {
+    public static void main(String[] args) {
+//создание массива случайных чисел
+        Random random = new Random();
+        int i = 0;
+        int[] array = new int[100];
+        while (i < 100) {
+            array[i] = random.nextInt(100);
+            i++;
+            }
+//создание строки из массива интов, а потом массива строчных символов
+        String tempArray = Arrays.toString(array);
+        String temp[] = tempArray.split(" ");
+        for (int x =0; x < temp.length; x+=11) {
+            temp[x] = "\n";
+        }
+        //отсеивание запятых из массива и разбивка пробелами
+        String temp2 = Arrays.toString(temp).replaceAll("[^0-9\n]", " ");
+        System.out.println(temp2);
+
+        createNumbersFile(temp2);
+        createOddNumbersFile();
+    }
+//запись массива temp2 в файл
+    private static void createNumbersFile(String data) {
+        try (Writer writer = new FileWriter("C:/Users/Zhenya/Desktop/JavaHomeWork/numbers.txt")){
+            writer.write(data);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+//чтание из файла numbers, сортировка на четность, создание массива интов и запись его в массив стринг с разбивкой по строкам
+    private static void createOddNumbersFile() {
+        Path path = Paths.get("C:/Users/Zhenya/Desktop/JavaHomeWork/numbers.txt");
+        Path path1 = Paths.get("C:/Users/Zhenya/Desktop/JavaHomeWork/odd-numbers.txt");
+        String[] b = new String[60]; //создание массива для обработки прочитанного файла
+        int i = 0;
+
+        try (Scanner scan = new Scanner(path)) {
+            while (scan.hasNextLine()) { //чтение
+              int d = scan.nextInt();
+                if (d % 2 == 0) { //сортировка
+                    i++;
+                    b[i] = Integer.toString(d); //массив стринг
+                    for (int x =0; x < b.length; x+=11)  //разбивка на строки
+                        b[x] = "\n";
+                }
+            }
+        } catch(Exception ex){
+                System.out.println(Arrays.toString(b).replaceAll("[^0-9\n]", " "));
+                System.out.println("Файл прочитан, отсортирован и перезаписан в odd-numbers.txt");
+            }
+//перевод b[i] в строку, отсеивание запятых и разбивка пробелами
+        String z = Arrays.toString(b).replaceAll("[^0-9\n]", " ");
+//запись массива строк в файл одд-намберс
+        try (BufferedWriter writer = Files.newBufferedWriter(path1)) {
+            writer.write(z);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
