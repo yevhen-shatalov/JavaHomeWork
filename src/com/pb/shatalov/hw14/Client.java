ad0abe766953ed08e7ddd5dfa1a5add45b1cda19
yevhen-shatalov <zhekashatalov1986@gmail.com>
2022-01-07 20:20:46 +0200

    init commit

---
+++ src/com/pb/shatalov/hw14/Client.java
@@ -0,0 +1,93 @@
+package com.pb.shatalov.hw14;
+
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalTime;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader in; // поток чтения из сокета
+    private BufferedWriter out; // поток чтения в сокет
+    private BufferedReader inputUser; // поток чтения с консоли
+    private String nickname;
+
+    public Client(String addr, int port) {
+
+        try {
+            this.socket = new Socket(addr, port);
+        } catch (IOException e) {
+            System.err.println("Socket failed");
+        }
+        try {
+            // потоки чтения из сокета / записи в сокет, и чтения с консоли
+            inputUser = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.askNickname();
+            new Client.ReadMsg().start(); // нить читающая сообщения из сокета в бесконечном цикле
+            new Client.WriteMsg().start(); // нить пишущая сообщения в сокет приходящие с консоли в бесконечном цикле
+        } catch (IOException e) {
+        }
+
+    }
+
+    private void askNickname() {
+        System.out.print("Enter your name: ");
+        try {
+            nickname = inputUser.readLine();
+           // out.write("Hi " + nickname + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+        }
+    }
+
+    // нить чтения сообщений с сервера
+    private class ReadMsg extends Thread {
+        @Override
+        public void run() {
+
+            String str;
+            try {
+                while ((str = in.readLine()) != null) {
+
+                    if (str.equals("exit")) {
+                        System.out.println("disconnected");
+                        break;
+                    }
+                    System.out.println(str); // пишем сообщение с сервера на консоль
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    // нить отправляющая сообщения на сервер
+    public class WriteMsg extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                String userWord;
+                try {
+                    userWord = inputUser.readLine(); // сообщения с консоли
+                    if (userWord.equals("exit")) {
+                        out.write("exit");
+                        break;
+                    } else {
+                        out.write(nickname + " " + "(" + LocalTime.now() + "): " + userWord + "\n"); // отправляем на сервер
+                    }
+                    out.flush(); // чистим
+                } catch (IOException e) {
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Srarted, type 'exit' to stop");
+        String ipAddr = "localhost";
+        int port = 1234;
+        new Client(ipAddr, port);
+    }
+}
---
+++ src/com/pb/shatalov/hw14/Server.java
@@ -0,0 +1,87 @@
+package com.pb.shatalov.hw14;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    public static LinkedList<Handler> clients = new LinkedList<>();
+
+    static class Handler implements Runnable {
+        private final Socket socket;;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                System.out.println(Thread.currentThread().getName() + ": received request from client");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                System.out.println("Waiting message...");
+
+                String clientMessage;
+                while ((clientMessage = in.readLine()) != null) {
+
+                    if ("exit".equalsIgnoreCase(clientMessage)) {
+                        out.write("exit");
+                        socket.close();
+                        break;
+                    }
+
+                    for (Handler vr : clients) {
+                        vr.send(clientMessage);
+                    }
+
+                    System.out.println(clientMessage);
+                    Thread.sleep(100);
+                    out.flush();
+                }
+                out.close();
+                in.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        private void send(String msg) throws IOException {
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            try {
+                out.write(msg + "\n");
+                out.flush();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+        ExecutorService treadPool = Executors.newFixedThreadPool(4);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            clients.add(new Handler(clientSocket));
+            treadPool.submit(new Handler(clientSocket));
+            clientSocket.getInputStream();
+            clientSocket.getOutputStream();
+        }
+    }
+}
